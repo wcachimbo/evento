1c01b62c18d71d34e86edd682b6cba7bc402b088
wcachimbo <wcachimbo@smart-financial-systems.com>
2026-01-03 16:11:08 -0500

    fix: Repara la zona horaria del proyecto para bogota colombia

--- src/main/java/com/evento/EventoApplication.java
+++ src/main/java/com/evento/EventoApplication.java
@@ -1,13 +1,21 @@
 package com.evento;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class EventoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
+    }
 
 }
